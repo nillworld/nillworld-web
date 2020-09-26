1f04f244c1f85670047f475cf9babb654bb5c6e3
Nill <58133705+nillworld@users.noreply.github.com>
2020-09-27 00:25:31 +0900

    Create test.java

---
+++ nillworld/apple-clone-v3/java/test.java
@@ -0,0 +1,188 @@
+
+import java.util.Arrays;
+
+// public class test {
+    /* public static void main(String[] args) {
+            // String[] seoul = {"Kim", "dsfds"};
+            // String answer = "";
+            // for(int i = 0; i < seoul.length; i++){
+            //     if(seoul[i].equals("Kim")){
+                    
+            //         answer = "김서방은 " + i + "에 있다";
+            //         break;
+            //     }
+            // }
+            // System.out.println(answer);
+            // int[] a = new int[4];
+            // int[] b = {1, 2, 3, 4};
+            
+            // System.out.println(Arrays.toString(a));
+            // Integer k = new Integer(10);
+            // Integer e = 10;
+            // int f = k + e;
+            // Integer g = k + e;
+            // int c = k.intValue();
+            // int d = g.intValue();
+            // System.out.println(k);
+            // System.out.println(e);
+            // System.out.println(c);
+            // System.out.println(d);
+            // System.out.println(f);
+            // System.out.println(g);
+
+
+            // for(int i = 0; i < 0; ++i){
+            //     System.out.println("test1");
+            // }
+
+            // String test2 = "test2";
+            // String test3 = "test3";
+            // test2 = test3;
+            // // System.out.println(test2);
+            // // test2 = "test4";
+            // // System.out.println(test2);
+            // // System.out.println(test3);
+            // test3 = "test5";
+            // System.out.println(test2); 
+            // System.out.println(test3); 
+
+            // String[] test55 = {"a", "b"};
+            // String test663 = "";
+            // for(String test44 : test55){
+            //     test663 += test44;
+            //     System.out.println(test663);
+            // }
+
+            String a = "ee";
+            // String b = "ee";
+            String b = new String("ee"); 
+            System.out.println(a==b);
+            System.out.println(a.equals(b));
+            boolean Nillissmart = true;
+            System.out.println(Nillissmart);
+            String Nill = "정원일";
+            String smart = Nill;
+            System.out.println(smart.equals("정원일"));
+
+        } */
+// }
+
+class Nill{
+    /*   Nill == 정원일   */
+    private static Nill Nill;
+    
+    
+    private static Nill It_is_Nill(){
+        if(Developer() && Character("Passionate but careful")){
+            return Nill;
+        }
+        if(Developer() && Character("Diligent but witty")){
+            return Nill;
+        }
+        if(Developer() && Character("Creative but harmonious")){
+            return Nill;
+        }
+        if(Developer() && Person("Who want to work together")){
+            return Nill;
+        }
+        if(Developer() && Person(ee)){
+            return Nill;
+        }
+    }
+
+
+
+
+
+
+    private static Nill Developer(){
+        if(Character("ee") && Character("Passionate")){
+            return Nill;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static Nill is(){
+
+        String nill;
+        nill = passionable;
+        nill = handsome;
+        
+     return It_is_Nill;
+    }
+
+    public static Nill he_is(){
+        if(It_is_Nill==null){
+            It_is_Nill = new Nill();
+        }
+        return It_is_Nill;
+    }
+    if(Developer2() == Character(Passionate)){
+              return Nill;
+          }
+}
+public class test{
+    // static String passion;
+    public static void main(String[] args) {
+        Nill.is(passionable);
+        Nill.is.passion = 
+        System.out.println(nill_is == nill_is2);
+    }
+}
+
+class Nill{
+    boolean youAreHiringSomeone = false;
+    static boolean whoIsPassionable = true;
+    static boolean whoIsSmart = true;
+    
+
+    public static Hire(boolean youAreHiringSomeone){
+        
+        if(You.hire.Developer){
+          if(Developer2() == Character(Passionate)){
+              return Nill;
+          }
+          if(Developer2() == Character(Creative)){
+            return Nill;
+        }
+        }
+    }
+
+
+    if(you.)
+}
+
+public abstract class Nill2{
+    public void Nill(){
+
+    }
+}
+
+
+
+
+
+
+// class Solution {
+//     public String solution(String[] seoul) {
+//         String answer = "";
+//         String Kim = "Kim";
+//         for(int i = 0; i < seoul.length; i++){
+//             if(seoul[i] == Kim){
+                
+//                 answer = "김서방은 " + i + "에 있다";
+//                 break;
+//             }
+//         }
+//     return answer;        
+//     }
+// }
