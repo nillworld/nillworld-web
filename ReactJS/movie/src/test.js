3a54cbecd13d921b79e93561347100a85ec90248
nill <wwaosldk@gmail.com>
2021-01-28 23:42:39 +0900

    #1.5 Creat Router test js

---
+++ ReactJS/movie/src/test.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+
+const Home = () => {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Take me home~ Country road~</p>
+      </div>
+    );
+  };
+  
+  export default Home;
