d7c4dd19a62c974cfdd561ce0bcf950a66bb2e6c
nill <wwaosldk@gmail.com>
2021-05-03 02:44:32 +0900

    #0.3-5 cordingTest set

--- Python/test/naver.py
+++ Python/test/naver.py
@@ -5,3 +5,22 @@
 keyword_1 = 'mark'
 keyword_2 = 'love'
 keyword_3 = 'nillisawsome'
+
+sentence_1 = 'live simple life'
+sentence_2 = 'I will be what I want to be'
+sentence_3 = 'you got me thinking about when you were mine'
+
+rule_1 = [1, 2, 2, 3, 1]
+rule_2 = [1, 1, 3, 2, 4, 1]
+rule_3 = [3, 2, 4, 2, 1, 1, 1, 2, 1]
+
+
+def answer(keyword, sentence, rule):
+  
+  return 0
+
+
+
+
+
+answer()
