390913f64c07f63bb7ca04c6cd344894c8e7b892
nill <wwaosldk@gmail.com>
2021-05-03 03:10:40 +0900

    #0.3-6 cordingTest simple answer

--- Python/test/naver.py
+++ Python/test/naver.py
@@ -16,11 +16,21 @@ rule_3 = [3, 2, 4, 2, 1, 1, 1, 2, 1]
 
 
 def answer(keyword, sentence, rule):
-  
+  result = list(sentence)
+  insertIdx = 0
+  for i in range(len(rule)):
+    keywordIdx = i
+    insertIdx += i + rule[i]
+    if(len(keyword) < insertIdx):
+      keywordIdx = insertIdx % len(keyword)
+    result.insert(insertIdx, keyword[keywordIdx])
+  print(result)
   return 0
 
 
 
 
 
-answer()
+answer(keyword_1, sentence_1, rule_1)
+answer(keyword_2, sentence_2, rule_2)
+answer(keyword_3, sentence_3, rule_3)
