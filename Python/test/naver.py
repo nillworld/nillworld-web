5be505eda71fad8dbd5f7a891e721582fe62ddac
nill <wwaosldk@gmail.com>
2021-05-08 21:06:26 +0900

    #0.3-9 cordingTest solved

--- Python/test/naver.py
+++ Python/test/naver.py
@@ -11,7 +11,7 @@ sentence_2 = 'I will be what I want to be'
 sentence_3 = 'you got me thinking about when you were mine'
 
 rule_1 = [1, 2, 2, 3, 1]
-rule_2 = [1, 1, 3, 2, 4, 1]
+rule_2 = [1, 1, 3, 2, 4, 5]
 rule_3 = [3, 2, 4, 2, 1, 1, 1, 2, 1]
 
 
@@ -24,12 +24,16 @@ def answer(keyword, sentence, rule):
     if(len(keyword) - 1 < i):
       keywordIdx = (i + 1) % len(keyword)
     for j in range(insertIdx, insertIdx+ rule[i]):
-      if(result[j] == keyword[keywordIdx]):
-        result.insert(j, keyword[keywordIdx])
+      if result[j] == keyword[keywordIdx]:
+        result.insert(j + 1, keyword[keywordIdx])
+        print("duplication",i)
+        insertIdx += j + 1 - insertIdx
         break_check = True
         break
-    if break_check == True:
-      break
+    if break_check:
+      insertIdx += 1
+      continue
+    print("mono",i)
     insertIdx += rule[i]
     result.insert(insertIdx, keyword[keywordIdx])
     insertIdx += 1
