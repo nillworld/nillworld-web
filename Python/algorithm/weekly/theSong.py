cbb1361775180f2e65d87e64c0d2c9febb596d89
nill <wwaosldk@gmail.com>
2021-07-06 01:00:18 +0900

    #0.3-33  algorithm the song doing

--- Python/algorithm/weekly/theSong.py
+++ Python/algorithm/weekly/theSong.py
@@ -1,3 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17683
+
 def solution(m, musicinfos):
   answer = ''
   rememberM = len(m)
@@ -9,8 +11,29 @@ def solution(m, musicinfos):
 
   for musicinfo in musicinfos:
     eachMusic = list(musicinfo.split(","));
-    print(eachMusic)
-  return playLength
+    playHour = int(eachMusic[1][:2]) - int(eachMusic[0][:2])
+    playMinute = int(eachMusic[1][3:]) - int(eachMusic[0][3:]) + playHour* 60
+    doubleMelody = eachMusic[3] * 2
+    if len(doubleMelody) >= playMinute:
+      compareMelody = doubleMelody[:playMinute]
+    else:
+      k = playMinute// len(doubleMelody)
+      l = playMinute % len(doubleMelody)
+      compareMelody = eachMusic[3]*k + eachMusic[3][:l]
+    if m[len(m)-1] == '#':
+      if m in compareMelody:
+        return eachMusic[2]
+    else:
+      if m in compareMelody:
+        ######### 3번째 마지막 C랑 C# 구별 안되고 있음
+        if m + '#' not in compareMelody+ compareMelody[0]:
+          print(m)
+          return eachMusic[2]
+      
+    if m in compareMelody:
+      return eachMusic[2]
+  return "none"
+      
 
 
 
