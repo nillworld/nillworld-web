ab20bdf4767900d19329166f2401a157928935da
nill <wwaosldk@gmail.com>
2021-07-11 15:37:29 +0900

    #0.3-36  algorithm the song done

--- Python/algorithm/weekly/theSong.py
+++ Python/algorithm/weekly/theSong.py
@@ -1,31 +1,37 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17683
 
 def solution(m, musicinfos):
-  answer = 'none'
-  playtheMusic = 0
-  eachMusic = []
+  answer = '(None)' 
+  tempMusicLen = 0 
+  m = m.replace("C#","c")
+  m = m.replace("D#","d")
+  m = m.replace("F#","f")
+  m = m.replace("G#","g")
+  m = m.replace("A#","a")
   for musicinfo in musicinfos:
-    eachMusic = list(musicinfo.split(","));
-    playHour = int(eachMusic[1][:2]) - int(eachMusic[0][:2])
-    playMinute = int(eachMusic[1][3:]) - int(eachMusic[0][3:]) + playHour* 60
+    startTime = list(musicinfo.split(","))[0]
+    endTime = list(musicinfo.split(","))[1]
+    musicTitle = list(musicinfo.split(","))[2]
+    playMelody = list(musicinfo.split(","))[3]
+    playMelody = playMelody.replace("C#","c")
+    playMelody = playMelody.replace("D#","d")
+    playMelody = playMelody.replace("F#","f")
+    playMelody = playMelody.replace("G#","g")
+    playMelody = playMelody.replace("A#","a")
+    playHour = int(endTime[:2]) - int(startTime[:2])
+    playMinute = int(endTime[3:]) - int(startTime[3:]) + playHour* 60
 
-    if len(eachMusic[3]) < playMinute:
-      quotient = playMinute // len(eachMusic[3])
-      remainder = playMinute % len(eachMusic[3])
-      compareMelody = eachMusic[3]*quotient+eachMusic[3][:remainder]
+    if len(playMelody) < playMinute:
+      quotient = playMinute // len(playMelody)
+      remainder = playMinute % len(playMelody)
+      compareMelody = playMelody*quotient+playMelody[:remainder]
     else:
-      compareMelody = eachMusic[3][:playMinute]
-
-    if m[-1:] == '#':
-      if m in compareMelody:
-        if playtheMusic < playMinute:
-          answer = eachMusic[2]
-    else:
-      if m in compareMelody:
-        if m + '#' not in compareMelody+ compareMelody[0]:
-          if playtheMusic < playMinute:
-            answer = eachMusic[2]
-      
+      compareMelody = playMelody[:playMinute]
+    
+    if m in compareMelody:
+      if tempMusicLen < playMinute:
+        answer = musicTitle
+        tempMusicLen = playMinute
   return answer
       
 
