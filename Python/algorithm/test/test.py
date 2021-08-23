0706230eaba93c31ae704512604c1569c0b76077
nill <wwaosldk@gmail.com>
2021-08-23 23:04:13 +0900

    algorism recommendJob start

---
+++ Python/algorithm/test/test.py
@@ -0,0 +1,11 @@
+array= [0, 1]
+
+def test(array):
+  array = [1,2]
+
+test(array)
+print(array)
+
+
+# 전역 변수 활용 ///   lexcal scoping ????
+#  append는 가능.
---
+++ Python/algorithm/weekly/recommendJobPart.py
@@ -0,0 +1,22 @@
+# https://programmers.co.kr/learn/courses/30/lessons/84325
+
+# 점수	SI	CONTENTS	HARDWARE	PORTAL	GAME
+# 5	JAVA	JAVASCRIPT	C	JAVA	C++
+# 4	JAVASCRIPT	JAVA	C++	JAVASCRIPT	C#
+# 3	SQL	PYTHON	PYTHON	PYTHON	JAVASCRIPT
+# 2	PYTHON	SQL	JAVA	KOTLIN	C
+# 1	C#	C++	JAVASCRIPT	PHP	JAVA
+
+def solution(table, languages, preference):
+    answer = ''
+    return answer
+
+
+
+
+table = ["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"]
+languages = ["PYTHON", "C++", "SQL"]
+preference = [7, 5, 5]
+
+
+print(solution(table, languages, preference))
