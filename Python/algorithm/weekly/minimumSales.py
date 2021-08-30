5c878ee698d1253d2c271118a2d6e237f694e4ff
nill <wwaosldk@gmail.com>
2021-08-30 22:17:00 +0900

    algorism minimumSales start

---
+++ Python/algorithm/weekly/minimumSales.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/72416
+
+def solution(sales, links):
+    answer = 0
+    return answer
+
+
+salse = [14, 17, 15, 18, 19, 14, 13, 16, 28, 17]
+links = [[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]
+print(solution(salse, links))
