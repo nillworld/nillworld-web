509ec245468ac6d7570c97e54e23b0143168b811
nill <inventor_@tobesoft.com>
2021-09-02 11:30:09 +0900

    alorism miniumSales ing....

--- Python/algorithm/weekly/minimumSales.py
+++ Python/algorithm/weekly/minimumSales.py
@@ -2,21 +2,52 @@
 from collections import defaultdict
 
 def solution(sales, links):
-    dictionary = defaultdict(list)
+    # links를 번호순으로 정렬
+    links.sort(key=lambda x: (x[0], x[1]))
+    totalTeamNum = links[-1][0]
+    print(totalTeamNum)
+    # 팀원으로 구성된 새로운 배열 초기화 / 팀장이 index 0
+    teams = [[i] for i in range(totalTeamNum)]
+    print(teams)
+    # sales랑 계산하기 쉽게 번호 1 씩 빼서 팀원들 배열에 넣기
     for link in links:
-        dictionary[link[0]-1].append(link[1]-1)
-    teamCheck(0, dictionary)
-    print(dictionary)
+        teams[link[0]-1].append(link[1]-1)
+    print(teams)
+
+    for team in teams:
+        
+
+
+
+
+
+    # dictionary = defaultdict(list)
+    # for link in links:
+    #     dictionary[link[0]-1].append(link[1]-1)
+    # # teamCheck(0, dictionary)
+    # print(dictionary)
+
+    # for i in dictionary[0]:
+    #     if(i in dictionary):
+    #         leader = sales[i]
+    #         # sales와 dictionary 파싱 해야함.
+    #         others = min(sales[dictionary[i]],sales[i]) + min(sales[dictionary[0]], sales[0])
+    #         # others = sales[min(dictionary[i])] + sales[min(dictionary[0])]
+    #         print(others, leader)
+
+
+
+
     answer = 0
     return answer
 
-def teamCheck(checkKey, dictionary):
-    for member in dictionary[checkKey]:
-        if member in dictionary:
-            print(member)
-            if (checkKey+1 in dictionary):
-                print("??")
-                teamCheck(checkKey+1, dictionary)
+# def teamCheck(checkKey, dictionary):
+#     for member in dictionary[checkKey]:
+#         if member in dictionary:
+#             print(member)
+#             if (checkKey+1 in dictionary):
+#                 print("??")
+#                 teamCheck(checkKey+1, dictionary)
 
 
     # 각 팀 관계에서 (연결된)팀장 매출 vs 나머지 인원 중 각 팀 두명 매출의 최소값
@@ -24,11 +55,14 @@ def teamCheck(checkKey, dictionary):
     ##### 나머지 팀 연결에서 '두명 - 팀장'의 값이 최대 인 팀 또 처리.
 
 
+    # 팀 연결을 다 확인...?
+    
+
 sales = [5, 6, 5, 3, 4]
 links = [[1,3], [1,4], [2,5], [1,2]]
 #       1
 #   3   4   2
-#          4 5
+#           5
 
 
 # 팀을 만들자. team = [[1,4,2],[2,3,5]] 팀장은 team[n][0]에 위치.
