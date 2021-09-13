3093feec6090b338623429ebeaee24c02872bc7d
nillworld <wwaosldk@gmail.com>
2021-09-13 17:45:01 +0900

    kakao test4 just depth.... ㅠ

--- Python/algorithm/test/kakao4.py
+++ Python/algorithm/test/kakao4.py
@@ -3,24 +3,28 @@ import itertools
 def solution(n, info):
     answer = []
     score = 0
+    # 몇번째 과녁에 쏘았는지 경우의 수를 중복조합을 이용하여 배열로 나타냄
     ways = list(itertools.combinations_with_replacement(([i for i in range(11)]), n))
+    # print(ways)
     for way in ways:
         tempScore = 0
         tempInfo = [0]*11
+
+        # 경우의 수 배열 하나를 과녁 스코어에 매칭  ex) [1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0]
         for i in way:
             tempInfo[i] += 1
-        # print(tempInfo)
+
         for i in range(len(info)):
             if tempInfo[i] > info[i]:
                 tempScore += 10-i
             if tempInfo[i] <= info[i] and info[i] > 0:
                 tempScore -= 10-i
             
-            if tempScore > score:
-                score = tempScore
-                answer = tempInfo
+        if tempScore > score:
+            score = tempScore
+            answer = tempInfo
     print(score)
-    if answer <= 0:
+    if score <= 0:
         answer = [-1]
     return answer
 n = 5
