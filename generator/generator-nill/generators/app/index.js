f7bbd332c160c36e7b030dd1512321b6144c15b9
nillworld <wwaosldk@gmail.com>
2021-09-17 09:50:49 +0900

    divide generator repo

--- generator/generator-nill/generators/app/index.js
+++
@@ -1,9 +0,0 @@
-const Generator = require("yeoman-generator");
-const chalk = require("chalk");
-
-module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(`Welcome to the magnificent ${chalk.red("generator-test")} generator!`);
-  }
-};
