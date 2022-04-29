86bd7a95ab3feacc3a1210e545b8c9636e80751c
nillworld <wwaosldk@gmail.com>
2022-04-29 13:56:34 +0900

    repository organize

--- nodeJS/npmProject/start.js
+++
@@ -1,7 +0,0 @@
-const wellcome = require("nillworld");
-const thisfilefullname = document.URL.substring(
-  document.URL.lastIndexOf("/") + 1,
-  document.URL.length
-);
-
-wellcome(thisfilefullname);
